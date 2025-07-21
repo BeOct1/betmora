eedd7a7ff56fa037ab7010f6af202115500651b9
BeOct1 <bitrusmail@gmail.com>
2025-07-21 11:13:42 +0100

    fix backend

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,6 @@ app.use('/api/movies', movieRoutes)
 app.use('/api/watchlist', watchlistRoutes)
 app.use('/api/favorites', favoriteRoutes)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
