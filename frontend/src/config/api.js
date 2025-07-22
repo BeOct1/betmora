6014c01d82faba30dafd67cf1dc6828237c6eb8e
BeOct1 <bitrusmail@gmail.com>
2025-07-22 16:30:10 +0100

    vercel url updated

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // Determine the base URL based on the environment
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://betmorapp.vercel.app'
-  : 'http://localhost:5001';
+  ? 'https://betmorapp-edwards-projects-0bb04786.vercel.app'
+  : 'http://localhost:5000';
 
 // Create axios instance with base URL
 const api = axios.create({
