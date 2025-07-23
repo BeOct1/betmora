839d06b9a6c83f382d648cce17ebbf11f086255e
BeOct1 <bitrusmail@gmail.com>
2025-07-23 15:23:53 +0100

    fix frontend

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ app.use(cors({
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
